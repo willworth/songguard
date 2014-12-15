9921c9a825d7e75f1bc2bd3d39ed9fd6e2267d3e
Will <will@willworth.co.uk>
2014-12-15 19:24:31 +0100

    footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,9 @@
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
-
+<!--"When art critics get together they talk about Form and Structure and Meaning. When artists get together they talk about where you can buy cheap turpentine."
+-Pablo Picasso
+!-->
 </body>
 </html>
 
